f7c3a4633576bb1f580a4a1dc018a20489dc307d
Juan Esquivel <jesquivel@samasource.org>
2017-08-18 17:02:26 -0600

    Very basic spark file

---
+++ spark/basic_sql_reader.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Apache Spark program that uses a SQL query as data source to feed into spark.
+"""
+
+from pyspark.sql import SparkSession
+
+logFile = "/usr/local/spark/README.md"
+spark = SparkSession.builder.appName("BasicSQL").master("local").getOrCreate()
+logData = spark.read.text(logFile).cache()
+
+numAs = logData.filter(logData.value.contains('a')).count()
+numBs = logData.filter(logData.value.contains('b')).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+
+spark.stop()
